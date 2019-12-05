323edb8fbb976201b5d09832d076d4e5697d9a38
Fernanda Fulcherberguer <ffulcherberguer@lcs.on.ca>
2019-12-05 14:57:19 -0500

    Made the outlets and actions

--- MultipleChoice/ViewController.swift
+++ MultipleChoice/ViewController.swift
@@ -8,12 +8,30 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
+class ViewController: UIViewController, UITextViewDelegate {
+    
+    //MARK: Properties
+    //Made outlets to connect with my view controller
+    @IBOutlet weak var numberOfQuestions: UITextField!
+    @IBOutlet weak var studentAnswer: UITextField!
+    
+    //MARK: Methods
+    //Runs once after the view first loads
+     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        
+    
     }
+    
+    //MARK: Actions
+    //Made an action for the convert button
+    @IBAction func analyzeAnswers(_ sender: Any) {
+        
+        
+    }
+    
 
 
 }
